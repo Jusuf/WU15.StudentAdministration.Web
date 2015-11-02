33640e8d5e06d106bf5cecd54723e190bccdc13f
jusuf_b <jusuf_b@hotmail.com>
2015-11-02 22:03:40 +0100

    json-utilities som egen js fil

---
+++ WU15.StudentAdministration.Web/Scripts/core/json-utilities.js
@@ -0,0 +1,14 @@
+﻿var Utilities = new function Utilities() {
+
+    Utilities.formToJson = function (form) {
+        var jsonForm = {};
+        $("input", $(form)).each(function (index) {
+            jsonForm[$(this).attr("name")] = this.value;
+        });
+
+        return jsonForm;
+    }
+
+    return Utilities;
+}
+
